fdfdb20048483faf081b3baf923e853a7bf945d7
Vijayleo46 <vijaymartin72@gmail.com>
2025-09-09 05:37:19 +0530

    Make Farm_Direct a package

--- farm/wsgi.py
+++ farm/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_direct.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm.settings')
 
 application = get_wsgi_application()
